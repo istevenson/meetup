a9d098ab3d4a688b71ab515a892e756faa95ebcc
Imon Stevenson <imon.stevenson@gmail.com>
2013-11-13 10:46:52 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meetup::Application.config.secret_key_base = '9ae4358bae9b9b36931a2bcc8dbb92b5988805194f09a351385b1701a2a984384a910dc9880d88103a31bdc0b2097d71344d6af403245dbdf50de278e4a918b4'
