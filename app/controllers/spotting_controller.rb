fb662eca518a203d4a5af09acad996a4b4ccc93e
Imon Stevenson <imon.stevenson@gmail.com>
2013-11-15 14:02:47 -0500

    demo1

--- app/controllers/spotting_controller.rb
+++ app/controllers/spotting_controller.rb
@@ -4,28 +4,29 @@ class SpottingsController < ApplicationController
   def show
   end
 
-  def create
-    @spotting = Spotting.new
-    respond_to do |format|
-      format.html
-      format.json { render json: @spotting}
-    end
-  end
+  # def create
+  #   @spotting = Spotting.new
+  #   respond_to do |format|
+  #     format.html
+  #     format.json { render json: @spotting}
+  #   end
+  # end
 
-  def update
-    respond_to do |format|
-      if @board.update
-        format.html
-        format.json { render json: @spotting}
-      end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @board.update
+  #       format.html
+  #       format.json { render json: @spotting}
+  #     end
+  #   end
+  # end
 
-  private
-    def set_spotting
-      @spotting = Spotting.find(params[:id])
-    end
+  # private
+  #   def set_spotting
+  #     @spotting = Spotting.find(params[:id])
+  #   end
 
-    def spottings_params
-      params.require(:spotting).permit(:location, :user_name)
-    end
+  #   def spottings_params
+  #     params.require(:spotting).permit(:location, :user_name)
+  #   end
 end
