5986af88dbb79e5a9c6bc0e49c10e1ae6126fe5a
Imon Stevenson <imon.stevenson@gmail.com>
2013-11-21 18:22:44 -0500

    adding a create_table tag to migration

--- db/migrate/20131114204650_create_spottings.rb
+++ db/migrate/20131114204650_create_spottings.rb
@@ -1,6 +1,6 @@
 class CreateSpottings < ActiveRecord::Migration
   def change
-    change_table :spottings do |t|
+    create_table :spottings do |t|
       t.column :spotting_id, :integer
       t.column :user_name, :string
       t.column :lat, :float
